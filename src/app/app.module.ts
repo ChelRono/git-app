3d7eeebf9e301dc76d72dd5ff6f808e843bc073a
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-04-18 18:17:43 -0400

    made a few changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,33 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { GithubServiceService } from './github-service.service';
+import { GitService } from './git.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { OrganisationComponent } from './organisation/organisation.component';
 import { ReposComponent } from './repos/repos.component';
 import { TruncatePipe } from './truncate.pipe';
-import { ReposComponent } from './repos/repos.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    DashboardComponent,
-    OrganisationComponent,
     ReposComponent,
     TruncatePipe,
-    ReposComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [GithubServiceService],
+  providers: [GitService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
