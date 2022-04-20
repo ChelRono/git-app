df41abba383f08ec03bb66862a09c80c8b9d523d
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-04-19 21:38:32 -0400

    made changes in app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 import { UserComponent } from './user/user.component';
+import { OrgComponent } from './org/org.component';
 
 
 const routes: Routes = [
   { path: '', pathMatch:'full', redirectTo: '/' },
   { path: 'user', component: UserComponent },
-]
+  { path: 'org',  component: OrgComponent }
+];
 
 
 @NgModule({
